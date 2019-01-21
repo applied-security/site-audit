4126a5a4b5ec2eb1f6dccacff195851c13dabca4
Oliver Stenbom <ors15@doc.ic.ac.uk>
2019-01-21 09:17:21 +0000

    Added initial content & background scripts

---
+++ background.js
@@ -0,0 +1,7 @@
+// chrome.webNavigation.onCompleted.addListener(function(details) {
+//   console.log("crazy things may happen here!");
+//   console.log(details.url);
+// }, {url: [{urlMatches : "*"}]});
+// That didn't work
+
+
