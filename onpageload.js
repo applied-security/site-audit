15548ad3753379b98e8557f93801051e868fde08
Oliver Stenbom <ors15@doc.ic.ac.uk>
2019-01-21 10:36:13 +0000

    Checking for scripts loaded over http

--- onpageload.js
+++ onpageload.js
@@ -1,1 +1,25 @@
-console.log("hello from SiteAudit!!");
+
+function checkForInsecureScriptLoading() {
+  let scriptElems = document.getElementsByTagName("script");
+
+  for (let i = 0; i < scriptElems.length; i++) {
+    let scriptElem = scriptElems[i];
+    let srcUrl = scriptElem.getAttribute("src");
+    if (srcUrl !== null) {
+      let lowerUrl = srcUrl.toLowerCase();
+      if (lowerUrl.indexOf("http://") === 0) {
+        vunerability = "Script loaded over unsafe http: " + lowerUrl;
+        logSecurityVunerability(vunerability, 2);
+      }
+    }
+  }
+}
+
+
+function logSecurityVunerability(vunerability, level) {
+  // TODO: send a message to the popup
+  console.log("Found vunerability: " + vunerability + ". Level is: " + level);
+}
+
+// Begin vunerability testing
+checkForInsecureScriptLoading()
