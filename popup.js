d70676911f6858ab28dfd962ce0ff8fbbe04fa6f
Inusha Hapuarachchi <inusha@outlook.com>
2019-01-22 15:27:24 +0000

    styled error messages

--- popup.js
+++ popup.js
@@ -4,7 +4,12 @@ function getVulnerabilities() {
     body: 'all'
   }
   chrome.runtime.sendMessage(message, function(response) {
-      document.body.innerHTML = response.response;
+  	  var htmlBuilder = "<ul>"
+  	  for (var i = 0; i < response.response.length; i++) {
+		htmlBuilder += "<li><a style=\"color: orange\">" + response.response[i] + "</a></li>"  	  	
+  	  }
+      htmlBuilder += "</ul>"
+      document.body.innerHTML = htmlBuilder
   });
 }
 
